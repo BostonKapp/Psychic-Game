bd6ed5f4a0e6c78ccfa1f3365f9f2a0268e3b35a
Devon Galloway <Devon_Galloway5@hotmail.com>
2019-02-26 16:19:07 -0500

    somethign

---
+++ app.js
@@ -0,0 +1,9 @@
+// This is gonna count how many times you won
+var wins =0;
+// This is gonna count how many times you lost
+var losses =0;
+// This is how many guesses you have left
+var guessesleft = 13;
+// This is what letters you have guessed so far
+var lettersguessed = [];
+// The computer picks a random letster
