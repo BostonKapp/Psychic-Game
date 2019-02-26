fb27d8b2c14c93d0cf5153bdedc9ac07bf5aed85
Devon Galloway <Devon_Galloway5@hotmail.com>
2019-02-26 17:54:44 -0500

    pseudo-code

--- app.js
+++ app.js
@@ -6,4 +6,62 @@ var losses =0;
 var guessesleft = 13;
 // This is what letters you have guessed so far
 var lettersguessed = [];
-// The computer picks a random letster
+// The computer picks a random letter
+var computersletter = ""
+
+function letterGenerator (computersletter){
+   computersletter = alphabet[Math.floor(Math.random() * alphabet.length)]
+    
+}
+letterGenerator()
+console.log(computersletter)
+
+var alphabet = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p",
+"q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
+
+// Updated wins
+function winsupdate() {
+    wins++;
+}
+
+// Updated losses
+function lossupdate() {
+    losses++;
+}
+
+// Updated guesses
+function guessupdate() {
+    guessesleft--;
+}
+
+// Computer guess
+function computerguessupdate() {
+    
+}
+
+function reset() {
+    // resets the guessesleft
+    guessesleft = 13;
+    // resets the array of guessed[]
+    lettersguessed = [];
+
+}
+
+// When letter guessed is equal to computer guess; updates stats
+if (letterguessed == computersletter) {
+    winsupdate();
+    reset();
+
+}
+
+// When the user doesnt guess correct
+if (letterguessed !== computersletter){
+    guessesleft--
+}
+
+// When user runs out of guesses
+if (guessesleft <= 0){
+    reset();
+    lossupdate();
+
+}
